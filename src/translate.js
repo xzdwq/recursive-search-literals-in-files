397b18db239f11b38ba755c0d04f1cfb413e0d30
viktor.zhivodrov <viktor.zhivodrov@qvant.com>
2023-10-31 01:04:06 +0300

    added translate service

--- src/translate.js
+++ src/translate.js
@@ -7,32 +7,30 @@ const { log } = console;
 export const translate = async (text) => {
   let translateResult = '';
 
-  const axiosConfig = {
+  const apiData = {
+    model: 'text-davinci-002',
+    messages: [
+      {
+        role: 'system',
+        content: 'Вы переводите следующий текст с русского на английский:',
+      },
+      {
+        role: 'user',
+        content: text,
+      },
+    ],
+  };
+
+  const apiConfig = {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${config.translate.apiSecretKey}`,
     },
   };
-  if (config.translate.reqTimeoutMs) axiosConfig.timeout = config.translate.reqTimeoutMs;
+  if (config.translate.reqTimeoutMs) apiConfig.timeout = config.translate.reqTimeoutMs;
 
   try {
-    const response = await axios.post(
-      config.translate.url,
-      {
-        model: 'text-davinci-002',
-        messages: [
-          {
-            role: 'system',
-            content: 'Вы переводите следующий текст с русского на английский:',
-          },
-          {
-            role: 'user',
-            content: text,
-          },
-        ],
-      },
-      axiosConfig
-    );
+    const response = await axios.post(config.translate.url, apiData, apiConfig);
     translateResult = response.data.choices[0].message.content;
   } catch (e) {
     log(chalk.red('Translate error: '), e.toString(), e?.response?.statusText || '');
